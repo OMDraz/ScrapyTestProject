ac47efd11da1d72626e900b102161aa83ccd5659
Omar <omardraz94@gmail.com>
2022-08-10 20:01:14 -0700

    fixed the text and added colors

--- sqlcv/pages/index.js
+++ sqlcv/pages/index.js
@@ -4,27 +4,36 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div class="container bg-sky-900 min-h-screen max-w-screen-xl grid grid-cols-6 row-6">
-      <div class="bg-slate-900 col-span-6 space-x-24">
+    <div class="container bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-500 min-h-screen max-w-screen-xl grid grid-cols-6 row-6">
+      <div class="bg-gradient-to-r from-blue-400 to-blue-600 col-span-6 space-x-24">
         <div class="display:inline-block">logo</div>
         <div class="display:inline-block float-right">upload csv</div>
         <div class="display:inline-block float-right">about</div>
         <div class="display:inline-block float-right">contact us</div>
       </div>
-      <div class="col-span-3 row-span-4">
-        <h1>The easiest way to create SQL tables</h1>
-        <h1>
+      <div class="col-span-3 row-span-4 pl-14">
+        <h1 class="pt-24 font-roboto text-6xl">
+          The easiest way to create SQL tables
+        </h1>
+        <h1 class="pt-6 text-2xl">
           Transform CSVs into SQL code to create tables in seconds rather than
           minutes for free. Just follow these three steps
-          <ol>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
-          </ol>
         </h1>
+        <ol class="py-2">
+          <li class="py-4">1. Upload a CSV with columns and data</li>
+          <li class="py-4">
+            2. Decide what data type you want for each column, along with table
+            name
+          </li>
+          <li class="py-4">
+            3. Copy the generated code into your SQL server and run it!
+          </li>
+        </ol>
       </div>
       <div class="col-span-3 row-span-4">picture</div>
-      <div class="bg-slate-900	col-span-6">footer</div>
+      <div class="col-span-6 bg-gradient-to-r from-blue-400 to-blue-600">
+        footer
+      </div>
     </div>
   );
 }
