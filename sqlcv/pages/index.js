ee35ef2441c66036c7f59bb393027cc91e9d6166
Omar <omardraz94@gmail.com>
2022-08-25 08:23:53 -0700

    added header and footer

--- sqlcv/pages/index.js
+++ sqlcv/pages/index.js
@@ -4,14 +4,14 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div class="container bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-500 min-h-screen max-w-screen-xl grid grid-cols-6 row-6">
-      <div class="bg-gradient-to-r from-blue-400 to-blue-600 col-span-6 space-x-24">
+    <div class="container bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-500 min-h-screen max-w-screen-xl grid grid-cols-6 row-8">
+      <header class="col-span-6 md:items-center rounded-lg bg-gradient-to-r from-blue-400 to-blue-600 space-x-24">
         <div class="display:inline-block">logo</div>
         <div class="display:inline-block float-right">upload csv</div>
         <div class="display:inline-block float-right">about</div>
         <div class="display:inline-block float-right">contact us</div>
-      </div>
-      <div class="col-span-3 row-span-4 pl-14">
+      </header>
+      <div class="col-span-5 row-span-6 pl-14">
         <h1 class="pt-24 font-roboto text-6xl">
           The easiest way to create SQL tables
         </h1>
@@ -30,10 +30,10 @@ export default function Home() {
           </li>
         </ol>
       </div>
-      <div class="col-span-3 row-span-4">picture</div>
-      <div class="col-span-6 bg-gradient-to-r from-blue-400 to-blue-600">
+      <div class="col-span-2 row-span-6">picture</div>
+      <footer class="col-span-6 rounded-lg md:flex md:items-center md:justify-between bg-gradient-to-r from-blue-400 to-blue-600">
         footer
-      </div>
+      </footer>
     </div>
   );
 }
